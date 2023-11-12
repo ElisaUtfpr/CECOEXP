54dfcae54408d45bbaa1fa47fb2466d2053693e3
ElisaUtfpr <elisacichella@alunos.utfpr.edu.br>
2023-11-11 23:53:53 -0300

    Retirada de Insumos

--- src/screens/RetiradaInsumos.js
+++ src/screens/RetiradaInsumos.js
@@ -1,15 +1,54 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, TextInput, TouchableOpacity, Modal, FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Modal,
+  FlatList,
+  Alert,
+} from 'react-native';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { serverTimestamp  , getDatabase, ref, push, set, child, get } from 'firebase/database';
 
 const RetiradaInsumos = () => {
   const [isModalVisible, setModalVisible] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [user, setUser] = useState(null);
+  const [productList, setProductList] = useState([]);
 
-  const productList = [
-    'Produto A',
-    'Produto B',
-    'Produto C',
-  ];
+  const auth = getAuth();
+  const database = getDatabase();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      if (user) {
+        // Se o usuário estiver autenticado, busca a lista de produtos no banco
+        fetchProductList();
+      }
+    });
+
+    return () => unsubscribe();
+  }, [auth]);
+
+  const fetchProductList = async () => {
+    try {
+      const produtosRef = ref(database, 'insumos');
+      const snapshot = await get(produtosRef);
+
+      if (snapshot.exists()) {
+        // Se existirem produtos, atualiza a lista de produtos
+        const produtos = snapshot.val();
+        const productNames = Object.keys(produtos).map((key) => produtos[key].nome);
+        setProductList(productNames);
+      }
+    } catch (error) {
+      console.error('Erro ao buscar a lista de produtos:', error);
+    }
+  };
 
   const toggleModal = () => {
     setModalVisible(!isModalVisible);
@@ -20,7 +59,36 @@ const RetiradaInsumos = () => {
     toggleModal();
   };
 
+  const handleRetirarProduto = () => {
+    if (!selectedProduct || !quantity) {
+      Alert.alert(
+        'Campos obrigatórios',
+        'Preencha todos os campos para retirar o produto.'
+      );
+      return;
+    }
 
+    if (!user) {
+      Alert.alert('Erro', 'Usuário não autenticado.');
+      return;
+    }
+
+    const retiradasRef = ref(database, 'retiradas');
+
+    const novaRetiradaRef = push(retiradasRef);
+    set(novaRetiradaRef, {
+      produto: selectedProduct,
+      quantidade: parseInt(quantity, 10), // Convert to integer
+      usuario: user.email || 'emaildesconhecido@example.com', // Email do usuário
+      data: serverTimestamp(), // Data da retirada
+    });
+
+    setQuantity('');
+    setSelectedProduct('');
+    toggleModal();
+
+    Alert.alert('Sucesso', 'Retirada registrada com sucesso!');
+  };
 
   return (
     <View style={styles.container}>
@@ -33,9 +101,11 @@ const RetiradaInsumos = () => {
       <TextInput
         style={styles.input}
         keyboardType="numeric"
+        value={quantity}
+        onChangeText={(text) => setQuantity(text)}
       />
 
-      <TouchableOpacity style={styles.button} >
+      <TouchableOpacity style={styles.button} onPress={handleRetirarProduto}>
         <Text style={styles.buttonText}>Retirar Produto</Text>
       </TouchableOpacity>
 
@@ -63,6 +133,8 @@ const RetiradaInsumos = () => {
     </View>
   );
 };
+
+
 
 const styles = StyleSheet.create({
   container: {
